82a4c1f90e6204178caad355458983270514f05c
Jeanrenel92 <jea.darius@duocuc.cl>
2026-05-25 21:25:06 -0400

    Initial commit proyecto inicial

---
+++ src/main/java/cl/duoc/api_viaje/ApiViajeApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.api_viaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiViajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiViajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/api_viaje/ApiViajeApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.api_viaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiViajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
